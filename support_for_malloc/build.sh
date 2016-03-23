dc41d235db1dde5bcc7bbcef70ff16fe8c78b44b
ipritish <pritishjain2009@gmail.com>
2016-03-23 13:16:11 +0530

    malloc add

---
+++ support_for_malloc/build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+arm-none-eabi-gcc -O0 -mfpu=vfp -mfloat-abi=soft -march=armv6zk -mtune=arm1176jzf-s -nostartfiles -g -Wl,-T,rpi.x ledblink.c cstubs.c startup.c start.S -o kernel.elf
+arm-none-eabi-objcopy kernel.elf -O binary kernel.img
