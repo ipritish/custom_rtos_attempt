923f078456f5f466d793da61c779bf2815788ff2
ipritish <pritishjain2009@gmail.com>
2016-03-25 14:58:03 +0530

    working cmake

--- ledblink_cmake/CMakeFiles/Makefile.cmake
+++ ledblink_cmake/CMakeFiles/Makefile.cmake
@@ -12,17 +12,30 @@ SET(CMAKE_MAKEFILE_DEPENDS
   "CMakeFiles/2.8.12.2/CMakeSystem.cmake"
   "CMakeLists.txt"
   "toolchain-arm-none-eabi-rpi.cmake"
+  "/usr/share/cmake-2.8/Modules/CMakeASMCompiler.cmake.in"
   "/usr/share/cmake-2.8/Modules/CMakeASMInformation.cmake"
+  "/usr/share/cmake-2.8/Modules/CMakeCCompiler.cmake.in"
   "/usr/share/cmake-2.8/Modules/CMakeCInformation.cmake"
+  "/usr/share/cmake-2.8/Modules/CMakeClDeps.cmake"
   "/usr/share/cmake-2.8/Modules/CMakeCommonLanguageInclude.cmake"
+  "/usr/share/cmake-2.8/Modules/CMakeDetermineASMCompiler.cmake"
+  "/usr/share/cmake-2.8/Modules/CMakeDetermineCCompiler.cmake"
+  "/usr/share/cmake-2.8/Modules/CMakeDetermineCompiler.cmake"
+  "/usr/share/cmake-2.8/Modules/CMakeDetermineCompilerId.cmake"
+  "/usr/share/cmake-2.8/Modules/CMakeDetermineSystem.cmake"
   "/usr/share/cmake-2.8/Modules/CMakeExtraGeneratorDetermineCompilerMacrosAndIncludeDirs.cmake"
+  "/usr/share/cmake-2.8/Modules/CMakeFindBinUtils.cmake"
   "/usr/share/cmake-2.8/Modules/CMakeFindCodeBlocks.cmake"
   "/usr/share/cmake-2.8/Modules/CMakeForceCompiler.cmake"
   "/usr/share/cmake-2.8/Modules/CMakeGenericSystem.cmake"
+  "/usr/share/cmake-2.8/Modules/CMakeSystem.cmake.in"
   "/usr/share/cmake-2.8/Modules/CMakeSystemSpecificInformation.cmake"
+  "/usr/share/cmake-2.8/Modules/CMakeTestASMCompiler.cmake"
+  "/usr/share/cmake-2.8/Modules/CMakeTestCCompiler.cmake"
   "/usr/share/cmake-2.8/Modules/Compiler/GNU-ASM.cmake"
   "/usr/share/cmake-2.8/Modules/Compiler/GNU-C.cmake"
   "/usr/share/cmake-2.8/Modules/Compiler/GNU.cmake"
+  "/usr/share/cmake-2.8/Modules/MultiArchCross.cmake"
   "/usr/share/cmake-2.8/Modules/Platform/Generic.cmake"
   )
 
@@ -34,10 +47,13 @@ SET(CMAKE_MAKEFILE_OUTPUTS
 
 # Byproducts of CMake generate step:
 SET(CMAKE_MAKEFILE_PRODUCTS
+  "CMakeFiles/2.8.12.2/CMakeSystem.cmake"
+  "CMakeFiles/2.8.12.2/CMakeCCompiler.cmake"
+  "CMakeFiles/2.8.12.2/CMakeASMCompiler.cmake"
   "CMakeFiles/CMakeDirectoryInformation.cmake"
   )
 
 # Dependency information for all targets:
 SET(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/ledblink.c.dir/DependInfo.cmake"
+  "CMakeFiles/kernel.dir/DependInfo.cmake"
   )
