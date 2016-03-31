1a9953944e963ce9b853b32a3af39d707d5e0ac4
ipritish <pritishjain2009@gmail.com>
2016-03-31 10:45:40 +0530

    first draft

---
+++ scheduler/ledblink.c
@@ -0,0 +1,38 @@
+#include <string.h>
+#include <stdlib.h>
+
+#include "rpi-gpio.h"
+#include "rpi-armtimer.h"
+#include "rpi-systimer.h"
+#include "rpi-interrupts.h"
+
+/** Main function - we'll never return from here */
+void kernel_main( unsigned int r0, unsigned int r1, unsigned int atags )
+{
+    /* Write 1 to the LED init nibble in the Function Select GPIO
+       peripheral register to enable LED pin as an output */
+    RPI_GetGpio()->LED_GPFSEL |= LED_GPFBIT;
+
+    /* Enable the timer interrupt IRQ */
+    RPI_GetIrqController()->Enable_Basic_IRQs = RPI_BASIC_ARM_TIMER_IRQ;
+
+    /* Setup the system timer interrupt */
+    /* Timer frequency = Clk/256 * 0x400 */
+    RPI_GetArmTimer()->Load = 0x400;
+
+    /* Setup the ARM Timer */
+    RPI_GetArmTimer()->Control =
+            RPI_ARMTIMER_CTRL_23BIT |
+            RPI_ARMTIMER_CTRL_ENABLE |
+            RPI_ARMTIMER_CTRL_INT_ENABLE |
+            RPI_ARMTIMER_CTRL_PRESCALE_256;
+
+    /* Enable interrupts! */
+    _enable_interrupts();
+
+    /* Never exit as there is no OS to exit to! */
+    while(1)
+    {
+
+    }
+}
